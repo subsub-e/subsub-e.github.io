a20c55c8974e9ad7dc1e39edbdb8c57b364fe3e2
subsub-e <ab13696802@gmail.com>
2020-05-10 22:26:00 +0900

    6th

---
+++ 6th/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.webview;
+
+// 여기서부터 수정
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // 앱에서 표시할 url 입력
+        webView.loadUrl("http://subsub-e.github.io/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    //폰의 뒤로가기 버튼의 동작 입력
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
